7ca1d5e5c802c6521fbbcf386e7531b81190fbeb
Vladyslav Tuvaiev <vlad.tuvaev@gmail.com>
2024-11-04 16:37:30 +0100

    one folder

--- tf/Vlad-Tuvaiev_123/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "Region for the S3 bucket"
-  default = "us-west-2"  # Значение по умолчанию
-}
-
-variable "bucket_name" {
-  description = "Name for the S3 bucket"
-  default = "backetbacketbacketbacketbacketqqqwww"  # Значение по умолчанию
-}
--- tf/Vlad-Tuvaiev_default-folder-2222/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "Region for the S3 bucket"
-  default = "us-west-222"  # Значение по умолчанию
-}
-
-variable "bucket_name" {
-  description = "Name for the S3 bucket"
-  default = "my-example-bucket-22222"  # Значение по умолчанию
-}
