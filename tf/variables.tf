52863986929ff8ef5d408cf7178d9c4c20c3ff24
Vlad Tuvaiev <103210833+Vlad-Tuvaiev@users.noreply.github.com>
2024-11-04 12:55:40 +0200

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,7 +1,9 @@
 variable "region" {
   description = "Region for the S3 bucket"
+  default     = "us-west-1"  # Значение по умолчанию
 }
 
 variable "bucket_name" {
   description = "Name for the S3 bucket"
+  default     = "my-example-bucket"  # Значение по умолчанию
 }
