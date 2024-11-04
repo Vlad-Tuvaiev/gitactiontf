c7676ad4b8be8b2122b0ecdb92ff8f3fd5f441a3
Vlad Tuvaiev <103210833+Vlad-Tuvaiev@users.noreply.github.com>
2024-11-04 13:38:50 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,11 +1,3 @@
-variable "region" {
-  description = "Region for the S3 bucket"
-}
-
-variable "bucket_name" {
-  description = "Name for the S3 bucket"
-}
-
 provider "aws" {
   region = var.region
 }
