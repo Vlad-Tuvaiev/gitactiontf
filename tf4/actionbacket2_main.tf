c4179607124d8ef330d791d1c28f750a6135597b
Vladyslav Tuvaiev <vlad.tuvaev@gmail.com>
2024-11-05 11:45:46 +0100

    rmstate

--- tf4/actionbacket2_main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "backetsactionomtest2" {
-  bucket = "backetsactionomtest2"
-  acl    = "private"
-}
--- tf4/actionbacket_main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "backetsactionomtest1" {
-  bucket = "backetsactionomtest1"
-  acl    = "private"
-}
