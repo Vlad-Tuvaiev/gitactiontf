060a39d918843c33388bd493ba3201b73829c092
Vlad Tuvaiev <103210833+Vlad-Tuvaiev@users.noreply.github.com>
2024-11-05 14:33:33 +0200

    Create provider.tf

---
+++ tf5/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "remoutestatetf"
+    key     = "terraform.tfstate"
+    region  = "us-west-1"
+    encrypt = true
+  }
+}
